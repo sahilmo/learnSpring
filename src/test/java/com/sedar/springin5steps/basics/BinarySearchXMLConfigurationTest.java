848d7852cf18a60feca20505614c149847382a8a
sahil <sahil_mohammad@outlook.com>
2019-07-18 15:12:32 +0400

    1.Define a xml configuration
    2.Define a separate test xml configuration

---
+++ src/test/java/com/sedar/springin5steps/basics/BinarySearchXMLConfigurationTest.java
@@ -0,0 +1,28 @@
+package com.sedar.springin5steps.basics;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.sedar.springin5steps.SpringIn5StepsBasicApplication;
+
+//load the context
+@RunWith(SpringRunner.class)
+@ContextConfiguration(locations="/testContext.xml")
+public class BinarySearchXMLConfigurationTest {
+
+	// get the bean from context
+	@Autowired
+	BinarySearchImpl binarySearch;
+
+	@Test
+	public void testBasicScenario() {
+	 int actualResult =	binarySearch.binarySearch(new int[] {}, 3);
+	 assertEquals(3, actualResult);
+	}
+
+}
